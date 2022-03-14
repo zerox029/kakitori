55de2935fc3a529bf467d8d2badcd9f913fe6c79
Étienne Plante <zerox029@gmail.com>
2022-03-14 10:11:44 -0400

    Fetching words for database

--- client/src/Components/StudyPage/StudyPage.tsx
+++ client/src/Components/StudyPage/StudyPage.tsx
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 
 import QuestionCard from "./QuestionCard";
 import AnswerCard from "./AnswerCard";
 
+interface IWord {
+  _id: string;
+  word: string;
+  reading: string;
+  translation: string;
+  level: number;
+}
+
 const StudyPage: React.FC = () => {
-  const [hasAnswered, setHasAnswered] = useState(false);
+  const [hasAnswered, setHasAnswered] = useState<boolean>(false);
+  const [wordList, setWordList] = useState<Array<IWord>>([]);
+  const [currentWord, setCurrentWord] = useState<IWord>();
+
+  useEffect(() => {
+    getWordList();
+  }, []);
+
+  useEffect(() => {
+    setNewWord();
+  }, [wordList]);
+
+  const getWordList = async () => {
+    const url = `http://localhost:5000/vocab/level/5`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setWordList(data);
+  }
+
+  const setNewWord = () => {
+    console.log(wordList)
+    const word = wordList[Math.floor(Math.random() * wordList.length)];
+    setCurrentWord(word);
+  }
 
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       {hasAnswered ? 
-        <AnswerCard word="言葉" reading="ことば" clickHandler={setHasAnswered} /> :
-        <QuestionCard reading="ことば" translation="word" clickHandler={setHasAnswered} />
+        <AnswerCard word={currentWord?.word ?? ""} reading={currentWord?.reading ?? ""} clickHandler={setHasAnswered} /> :
+        <QuestionCard reading={currentWord?.reading ?? ""} translation={currentWord?.translation ?? ""} clickHandler={setHasAnswered} />
       }  
     </Box>
   )
