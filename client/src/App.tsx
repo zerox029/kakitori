318f88363f426e817a43b6e1c8040fae73053ccb
Étienne Plante <zerox029@gmail.com>
2022-03-22 16:58:25 -0400

    Added a nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import StudyPage from "./Components/StudyPage/StudyPage";
 import StatsPage from "./Components/StatsPage/StatsPage"
 
 import './App.css';
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar/>
         <Routes>
-          <Route path="/" element={<StudyPage />} />
-          <Route path="/stats" element={<StatsPage />} />
+          <Route path="/study" element={<StudyPage />} />
+          <Route path="/statistics" element={<StatsPage />} />
+          <Route path="*" element={<Navigate to="/study" />}/>
         </Routes>
       </Router>
     </div>
